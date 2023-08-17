3da088793cfa969ffbbac6d80275dbc03942b765
pramdevgan <vpramatma@gmail.com>
2023-08-18 00:23:53 +0530

    Safe Text (used escape and format_html)

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
 from django import template
+from django.utils.html import escape, format_html
+from django.utils.safestring import mark_safe
+
 
 user_model = get_user_model()
 
@@ -17,4 +20,10 @@ def author_details(author):
     else:
         name = f"{author.username}"
 
-    return name
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+    return format_html("{}{}{}", prefix, name, suffix)
